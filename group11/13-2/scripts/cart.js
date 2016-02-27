a15b51c7d0e193c93ce7804c0d7953bd0354f776
Alex M <am@Alexs-MacBook-Pro.local>
2016-02-27 14:21:49 +0200

    add lesson 13

--- group11/13-2/scripts/cart.js
+++ group11/13-2/scripts/cart.js
@@ -17,6 +17,11 @@ function Cart(sSelector, sCartSelector) {
         var orderForm = $(this), //при клике на submit попадает вся форма
             currentGood = orderForm.closest('.b-good'), //строка tr с даным товаром. по форме находим ближайший tr с класом b-good
             addedGood = c.put(currentGood); //для розмещения товара в корзине
+        c.list.show();
+        c.goods[addedGood.getID()] = orderForm.find('.b-order-form__quantity').val();
+
+
+        $.cookie('cartGoods', c.goods, { 'expires': 7, 'path': '/' });
     };
 
     c.put = function(currentGood) { //розмещает в списке заказов
@@ -24,11 +29,75 @@ function Cart(sSelector, sCartSelector) {
             good_id_class = 'b-good_id_' + addedGood.getID(),
             existingGood = c.list.find('.' + good_id_class);
         currentGood.addClass('b-good_in-cart');
-
+        if (existingGood.length) {
+            c.copyData(currentGood, existingGood, ['.b-order-form__quantity']);
+        } else {
+            //находим первую строку в корзине, какая пустая и клонируем ее
+            var newGood = c.list.find('.b-good:first-child').clone();
+            //добавляем клас с айди
+            newGood.addClass(good_id_class);
+            //копируем с текущего товара в каталоге в новый товар, который будет в корзине
+            c.copyData(currentGood, newGood, ['.b-good__image', '.b-good__name', '.b-order-form__quantity', '.b-good__price']);
+            c.list.find('.b-goods').append(newGood);
+            c.bindMinicartEvents(newGood);
+        }
+        return addedGood;
     };
 
     c.load = function() { //загрузка товаров из cookies
+        c.goods = $.cookie('cartGoods');
+        if (c.goods) {
+            $.each(c.goods, function(goods_id, quantity) {
+                var currentGood = c.find('.b-good_id_' + goods_id);
+                currentGood.find('.b-order-form__quantity').val(quantity);
+                c.put(currentGood);
+            });
+        }
 
+    };
+    c.openCart = function(event) {
+        c.list.stop().slideToggle();
+    };
+
+    c.increseQuantity = function() {
+        c.changeQuantity(this, 1);
+    };
+
+    c.decreseQuantity = function() {
+        c.changeQuantity(this, -1);
+    };
+
+    c.changeQuantity = function(oBtn, iShift) {
+        var quantityTextField = $(oBtn).siblings('.b-order-form__quantity'),
+            currentQuantity = parseInt(quantityTextField.val()) + iShift;
+        if (currentQuantity > 0) {
+            quantityTextField.val(currentQuantity);
+        } else {
+            alert('Маловато будет! Маловато!!')
+        }
+    };
+
+    c.bindMinicartEvents = function(newGood) {
+        // 58. назначаем на элементы первой строки корзины (муляж - всё пустое) методы, как и в каталоге (+,-), и плюс к этому - удаление товара
+
+        //59 .end() - встроенный метод jQuery, который позволяет не дублировать объект newGood? в котором ищем элемент и назначаем на него метод
+        newGood
+            .find('.b-good__delete').bind('click', c.del)
+            .end() // встроенный метод, который позволяет не дублировать объект newGood в котором ищем элемент и назанчаем на него метод
+            .find('.b-order-form__plus').bind('click', c.increaseQuantity)
+            .end()
+            .find('.b-order-form__minus').bind('click', c.decreaseQuantity)
+            .end()
+            .find('.b-order-form').bind('submit', c.add).end();
+    };
+    c.del = function() {
+        var currentGood = $(this).closest('.b-good'),
+            goodDel = new Good(currentGood),
+            goodId = goodDel.getID();
+        c.list.find('.b-good_id_' + goodId).remove();
+        c.find('.b-good_id_' + goodId).removeClass('b-good_in-cart');
+        delete c.goods[goodId];
+        $.cookie('cartGoods', c.goods, { 'expires': 7, 'path': '/' });
     };
 
     c.main = function() {
@@ -40,10 +109,15 @@ function Cart(sSelector, sCartSelector) {
             c.order = c.cart.find('.b-minicart__order');
             c.quantity = c.cart.find('.b-minicart__quantity');
             c.total = c.cart.find('.b-minicart__total');
+            c.goods = { /* id:quantity */ };
+            $.cookie.json = true;
 
-
+            c.load();
             //События
             c.find('.b-order-form').bind('submit', c.add);
+            c.button.click(c.openCart);
+            c.find('.b-order-form__plus').bind('click', c.increseQuantity);
+            c.find('.b-order-form__minus').bind('click', c.decreseQuantity);
 
         }
         //-------------
--- group11/13-2/scripts/component.js
+++ group11/13-2/scripts/component.js
@@ -1,18 +1,31 @@
 function Component() {
-	this.elem = null; //$(sSelector)
-	
-	//метод для иницивлизации главного свойства this.elem
-	this.init = function(sSelector) {
-		this.elem = $(sSelector);
-		if (!this.elem.length) {
-			alert('Can\'n access element by selector: ' + sSelector);
-		};
-	};
-	this.find = function(sProp) {
-		var find_result = this.elem.find(sProp);
-		if (find_result.length) {
-			return find_result;
-		} else alert('Can\'n access element by selector: ' + sProp);
-		
-	};
+    this.elem = null; //$(sSelector)
+
+    //метод для иницивлизации главного свойства this.elem
+    this.init = function(sSelector) {
+        this.elem = $(sSelector);
+        if (!this.elem.length) {
+            alert('Can\'n access element by selector: ' + sSelector);
+        };
+    };
+    this.find = function(sProp) {
+        var find_result = this.elem.find(sProp);
+        if (find_result.length) {
+            return find_result;
+        } else alert('Can\'n access element by selector: ' + sProp);
+    };
+    this.copyData = function(oSource, oDestination, aFieldNames) {
+        $.each(aFieldNames, function(i, fieldName) {
+            var oSourceElement = oSource.find(fieldName),
+                oDestinationElement = oDestination.find(fieldName),
+                tagName = oSourceElement.prop('tagName'); //имя тега которое соответсвует считаному елементу
+            if (tagName == 'IMG') {
+                oDestinationElement.attr('src', oSourceElement.attr('src'));
+            } else if (tagName == 'INPUT' || tagName == 'TEXTAREA') {
+                oDestinationElement.val(oSourceElement.val());
+            } else {
+                oDestinationElement.html(oSourceElement.html());
+            }
+        });
+    };
 };
